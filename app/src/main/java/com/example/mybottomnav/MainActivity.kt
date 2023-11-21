fd9743b1c1fb9add47ba8386c915a97926968012
RanaHisham18 <hishamrana00@gmail.com>
2023-11-21 10:20:18 +0200

    initial commit

---
+++ app/src/main/java/com/example/mybottomnav/MainActivity.kt
@@ -0,0 +1,18 @@
+package com.example.mybottomnav
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.mybottomnav.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        var binding = ActivityMainBinding.inflate(layoutInflater)
+        var view = binding.root
+        setContentView(view)
+       binding.bottomNavigation.getMenu().findItem(R.id.page_1)
+        var badge = binding.bottomNavigation.getOrCreateBadge(R.id.page_1)
+        badge.isVisible = true
+
+    }
+}
